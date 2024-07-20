9dd10bd01f9afbcf2adf67022b46406b5eeff4c6
davidchanzz <tovliskatsi@gmail.com>
2024-07-20 18:10:40 +0400

    Init

---
+++ src/main/java/com/iteratia/titanicquest/TitanicQuestApplication.java
@@ -0,0 +1,13 @@
+package com.iteratia.titanicquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanicQuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TitanicQuestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iteratia/titanicquest/TitanicQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iteratia.titanicquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanicQuestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
